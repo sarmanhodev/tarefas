e2e741e64372d757dfe2c5bd96497bd5767f1c9b
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2024-02-20 14:21:50 -0300

    Update automatico.py

--- automatico.py
+++ automatico.py
@@ -1,15 +1,21 @@
 import schedule
 import time
+import threading
 import requests
 
-
 def atualizar_dado():
-    response = requests.get('https://lista-tarefas-uvq7.onrender.com/atualizar_registro_expirado')
+    url = 'https://lista-tarefas-uvq7.onrender.com//atualizar_registro_expirado'
+    response = requests.get(url)
     print(response.text)
 
-# Agende a execução a cada 24 horas
-schedule.every(2).hours.do(atualizar_dado)
+# Agende a execução a cada 4 horas (ajuste conforme necessário)
+schedule.every(4).hour.do(atualizar_dado)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Função para iniciar o agendamento em uma thread separada
+def iniciar_agendamento():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# Inicie o agendamento em uma thread separada
+threading.Thread(target=iniciar_agendamento).start()
