0f652413e47ba2ad06c792d4143d455575813533
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2024-03-18 09:10:02 -0300

    Update automatico.py

--- automatico.py
+++ automatico.py
@@ -9,8 +9,8 @@ def atualizar_dado():
     print(response.text)
 
 # Agende a execução a cada 4 horas (ajuste conforme necessário)
-schedule.every(10).seconds.do(atualizar_dado)
-
+schedule.every(240).seconds.do(atualizar_dado)
+print("\nTarefa funcionou\n")
 # Função para iniciar o agendamento em uma thread separada
 def iniciar_agendamento():
     while True:
